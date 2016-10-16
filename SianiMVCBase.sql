f5f8b6ccc9f4998452c58d6f711005e6ff8ca65b
Felideo <felideo@gmail.com>
2016-10-16 20:36:10 -0200

    Ajuste pagina de configurações!

--- SianiMVCBase.sql
+++ SianiMVCBase.sql
@@ -37,3 +37,6 @@ VALUES
 
 INSERT INTO `usuario` VALUES ('1', 'felideo@gmail.com', '12345', '0', '1');
 
+ALTER TABLE `modulo`
+    ADD COLUMN `submenu` 		varchar(64) NULL AFTER `nome`,
+    ADD COLUMN `submenu_icone` 	varchar(64) NULL AFTER `submenu`;
